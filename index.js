894dbf22a33e53246998cbae85a57c90430d3430
softwarejr <richardjr.magsalay@g.msuiit.edu.ph>
2022-04-26 23:07:57 +0800

    Add index.js file.

---
+++ index.js
@@ -0,0 +1,5 @@
+const sections = document.querySelector('.section');
+const controls_container = document.querySelectorAll('.controls');
+const control_btns = document.querySelectorAll('.control-btn'); 
+const sections_container = document.querySelector('.main-content');
+
