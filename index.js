e9013f10911f5005eec4fcc913c8ad39cf8fd093
softwarejr <richardjr.magsalay@g.msuiit.edu.ph>
2022-04-27 22:13:52 +0800

    Add page transition function.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const controls_container = document.querySelectorAll('.controls');
 const control_btns = document.querySelectorAll('.control-btn'); 
 const sections_container = document.querySelector('.main-content');
 
+function  pageTransition() {
+    // When button clicked, add style to it to make it active.
+    for(let i = 0; i < control_btns.length; i++){
+        control_btns[i].addEventListener('click', () => {
+            let current_btn = document.querySelectorAll('.active-btn');
+            current_btn[0].className = current_btn[0].className.replace('active-btn', '')
+            this.className += 'active-btn'
+        } )
+    } 
+}
+pageTransition()
