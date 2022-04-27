e620d8c8ae3cb147b86e51c48533c7b7b693568d
softwarejr <richardjr.magsalay@g.msuiit.edu.ph>
2022-04-27 22:27:11 +0800

    bugfix: Fix pageTrasition function.

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const sections_container = document.querySelector('.main-content');
 function  pageTransition() {
     // When button clicked, add style to it to make it active.
     for(let i = 0; i < control_btns.length; i++){
-        control_btns[i].addEventListener('click', () => {
+        control_btns[i].addEventListener('click', function() {
             let current_btn = document.querySelectorAll('.active-btn');
             current_btn[0].className = current_btn[0].className.replace('active-btn', '')
-            this.className += 'active-btn'
+            this.className += ' active-btn'
         } )
     } 
 }
